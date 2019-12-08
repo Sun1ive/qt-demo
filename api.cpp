20cbf4337e66d86f534dc19af4c306bc70fb6ea0
Sun1ive <sunliveua@gmail.com>
2019-12-08 18:51:02 +0200

    clean

--- api.cpp
+++ api.cpp
@@ -6,10 +6,10 @@ Api::Api(QObject *parent) : QObject(parent)
 }
 
 void Api::sendRequest(const QString &apiStr,
-                            const handleFunc &funcSuccess,
-                            const handleFunc &funcError,
-                            Api::Type type,
-                            const QVariantMap &data)
+                      const handleFunc &funcSuccess,
+                      const handleFunc &funcError,
+                      Api::Type type,
+                      const QVariantMap &data)
 {
     QNetworkRequest request = createRequest(apiStr);
 
@@ -17,10 +17,13 @@ void Api::sendRequest(const QString &apiStr,
 
     switch (type) {
     case Type::POST: {
-        reply = manager->post(request, QByteArray("{'hello': 'world'}"));
+        QByteArray postDataByteArray = variantMapToJson(data);
+        reply = manager->post(request, postDataByteArray);
+
         break;
     } case Type::GET: {
         reply = manager->get(request);
+
         break;
     } default:
         reply = nullptr;
@@ -55,3 +58,65 @@ QByteArray Api::variantMapToJson(QVariantMap data)
 
     return postDataByteArray;
 }
+
+QNetworkRequest Api::createRequest(const QString &apiStr)
+{
+    QNetworkRequest request;
+    request.setUrl(QUrl(apiStr));
+    request.setRawHeader("Content-Type","application/json");
+
+    return request;
+}
+
+QJsonObject Api::parseReply(QNetworkReply *reply)
+{
+    QJsonObject jsonObj;
+    QJsonDocument jsonDoc;
+    QJsonParseError parseError;
+
+    auto replyText = reply->readAll();
+
+    jsonDoc = QJsonDocument::fromJson(replyText, &parseError);
+
+    if(parseError.error != QJsonParseError::NoError){
+        qDebug() << replyText;
+        qWarning() << "Json parse error: " << parseError.errorString();
+    }else{
+        if(jsonDoc.isObject())
+            jsonObj  = jsonDoc.object();
+        else if (jsonDoc.isArray())
+            jsonObj["non_field_errors"] = jsonDoc.array();
+    }
+    return jsonObj;
+}
+
+bool Api::onFinishRequest(QNetworkReply *reply)
+{
+    auto replyError = reply->error();
+
+    if (replyError == QNetworkReply::NoError ) {
+        int code = reply->attribute(QNetworkRequest::HttpStatusCodeAttribute).toInt();
+        if ((code >=200) && (code < 300)) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+void Api::handleQtNetworkErrors(QNetworkReply *reply, QJsonObject &obj)
+{
+    auto replyError = reply->error();
+    if (!(replyError == QNetworkReply::NoError ||
+          replyError == QNetworkReply::ContentNotFoundError ||
+          replyError == QNetworkReply::ContentAccessDenied ||
+          replyError == QNetworkReply::ProtocolInvalidOperationError
+          ) ) {
+        qDebug() << reply->error();
+
+        obj[KEY_QNETWORK_REPLY_ERROR] = reply->errorString();
+    } else if (replyError == QNetworkReply::ContentNotFoundError) {
+        obj[KEY_CONTENT_NOT_FOUND] = reply->errorString();
+    }
+}
+
